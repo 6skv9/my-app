a836a3f32a5a0b704e7c12878856575b32e67a19
avasanth123456-wq <avasanth123456@gmail.com>
2025-10-14 16:35:59 +0530

    Vasant First commit

--- src/component/Home.js
+++ src/component/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     const navigate = useNavigate();
 
     const logout = () => {
-        localStorage.remove("token");
+        // localStorage.remove("token");
         navigate("/login");
     }
 
